e43d9374ddb39cf8f0e5b52c13f287ef5e1acd3c
John Comeau <jc@unternet.net>
2024-01-04 13:36:17 -0800

    help automate build

---
+++ debian.mk
@@ -0,0 +1,13 @@
+PREFIX ?= /usr/local
+all: dependencies python
+python: Makefile
+	$(MAKE) -f $<
+Makefile: configure
+	./$< --prefix=$(PREFIX)
+dependencies:
+	sudo apt-get install build-essential gdb lcov pkg-config \
+	 libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
+	 libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
+	 lzma lzma-dev tk-dev uuid-dev zlib1g-dev
+%: Makefile
+	$(MAKE) -f $< "$@"
